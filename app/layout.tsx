a61517f95827662b304f984cff6a1b55a279fe9f
John Radojevich <johnathanradojevich@gmail.com>
2022-12-27 16:48:16 -0600

    basic working

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,51 @@
-import './globals.css'
+import "./globals.css";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
-      <body>{children}</body>
+      <body className="h-screen">
+        <svg
+          width="40"
+          height="40"
+          viewBox="0 0 75 75"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="fixed top-4 left-4"
+        >
+          <g clipPath="url(#clip0_12_145)">
+            <path
+              d="M74.123 74.9852H66.4424L35.5154 44.0583V34.7744H57.1585L30.0791 7.69518V0L70.29 40.2107H39.3485L74.123 74.9852Z"
+              fill="black"
+            />
+            <path
+              d="M35.5152 20.8122V74.9852L0.740723 40.2108H8.42146L30.0789 61.8537V15.3906L35.5152 20.8122Z"
+              fill="black"
+            />
+          </g>
+          <defs>
+            <clipPath id="clip0_12_145">
+              <rect width="75" height="75" fill="white" />
+            </clipPath>
+          </defs>
+        </svg>
+        <svg
+          width="40"
+          height="40"
+          viewBox="0 0 32 32"
+          xmlns="http://www.w3.org/2000/svg"
+          className="fixed top-4 right-4"
+        >
+          <g id="Layer_6" data-name="Layer 6">
+            <path d="m29 17h-26a1 1 0 0 1 0-2h26a1 1 0 0 1 0 2zm1 6a1 1 0 0 0 -1-1h-26a1 1 0 0 0 0 2h26a1 1 0 0 0 1-1zm0-14a1 1 0 0 0 -1-1h-26a1 1 0 0 0 0 2h26a1 1 0 0 0 1-1z" />
+          </g>
+        </svg>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
