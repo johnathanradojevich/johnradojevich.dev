039c4cafcc76e44cd79ffbc04063da1310527c71
John Radojevich <johnathanradojevich@gmail.com>
2022-12-29 22:05:25 -0600

    mobile changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,17 @@
+"use client";
+
 import Image from "next/image";
 import MonogramImage from "../public/icons/monogram.svg";
+import MenuImage from "../public/icons/menu.svg";
+
+import {
+  ScrollDirection,
+  useScrollDirection,
+} from "./hooks/useScrollDirection";
+import LinkedinIcon from "../public/icons/linkedin.svg";
+import CodepenIcon from "../public/icons/codepen.svg";
+import PipeIcon from "../public/icons/pipe.svg";
+import StarIcon from "../public/icons/star.svg";
 
 import "./globals.css";
 
@@ -8,18 +20,39 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const scrollDirection = useScrollDirection();
   return (
     <html lang="en" className="font-plantin">
       <head />
-      <body className="h-screen">
+      <body className="flex min-h-screen flex-col">
         <Image
           src={MonogramImage}
           alt="John Radojevich"
-          height={40}
-          width={40}
-          className="fixed top-4 left-4"
+          height={30}
+          width={30}
+          className={`fixed top-4 left-4`}
         />
-        {children}
+        <Image
+          src={MenuImage}
+          alt=""
+          height={30}
+          width={30}
+          className={`fixed top-4 right-4 ${
+            scrollDirection === ScrollDirection.Up ? "invisible" : "visible"
+          }`}
+        />
+        <main>{children}</main>
+        <footer className="mt-auto py-4 text-center font-oak">
+          <div className="flex justify-center gap-2">
+            <Image src={LinkedinIcon} alt="GitHub" width={25} height={25} />
+            <Image src={CodepenIcon} alt="Codepen" width={25} height={25} />
+          </div>
+          <span className="text-xs">&copy; 2022 John Radojevich</span>
+          <div className="gap-2s flex justify-center">
+            <Image src={StarIcon} alt="" width={15} height={15} />
+            <Image src={PipeIcon} alt="" width={15} height={15} />
+          </div>
+        </footer>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,38 @@
-"use client";
-
-import { useScrollDirection } from "./hooks/useScrollDirection";
-
 export default function Home() {
-  const scrollDirection = useScrollDirection();
   return (
-    <main className="mt-14">
-      <div className="px-6 max-w-3xl mx-auto">
-        <div className="xl:max-w-xl">
-          <h1 className="text-2xl xl:text-4xl">
-            <span className=" text-3xl xl:text-5xl">Hello</span>, I&apos;m John.
-            <br></br>
-            Software Engineer & UI/UX Designer based in Milwaukee.
-            {scrollDirection}
-          </h1>
-          <div className="text-md xl:text-lg text-gray mt-4 tracking-wider leading-7">
-            <p>
-              I&apos;m a detail-oriented, responsible, and committed engineer,
-              with a get-it-done, on-time, and high-quality product spirit. I
-              have 3 years of experience defining, designing, implementing,
-              testing, and delivering complex web applications using a variety
-              of programming languages and technologies.
-            </p>
-            <p>
-              I have a distinctive and strategic creative process that allows me
-              to pinpoint the holistic scope of a product and evolve it by
-              adding features, innovations and values that meet both user needs
-              and business objectives. I craft beautiful user interfaces that
-              are both functional and minimal.
-            </p>
-            <p>
-              With my knowledge in both development and design, I prove a
-              valuable resource to any team.
-            </p>
-            <p>
-              I&apos;m currently a graduate student at the University of
-              Oklahoma, where I&apos;m pursuing a Master&apos; of Science in
-              Computer Science. I intend to focus on applying machine learning
-              and data mining to physics-based weather analytics and modeling,
-              particularly in the prediction of high-impact weather events.
-            </p>
-          </div>
+    <>
+      <div className="gradient">
+        <div className="text-gray mx-auto max-w-3xl px-6 pt-14 text-4xl text-neutral-500">
+          <h2 className="font-oak text-lg">Welcome, I&apos;m</h2>
+          <h1 className="text-black">John Radojevich.</h1>
+          <h3 className="leading-none">I build things for the web.</h3>
         </div>
       </div>
-    </main>
+      <div className="max-w-3xl px-6">
+        <h2 className="text-3xl ">
+          Hello, I&apos;m John, Software Engineer & UI/UX Designer based in
+          Milwaukee.
+        </h2>
+        <p className="leading-2 tracking-wider text-neutral-500">
+          I have 3 years of experience defining, designing, implementing,
+          testing, and delivering complex web applications using a variety of
+          programming langauges and technologies.
+        </p>
+        <p className="tracking-wider text-neutral-500">
+          I have a distinctive and strategic creative processs that allows me to
+          pinpoint the holistic scope of a product and evolve it by adding
+          features, innovations, and values that meet both user needs and
+          business objectives. I thrive when I&apos;m able to craft user
+          interfaces that make products simple, and enjoyable.
+        </p>
+        <p className="tracking-wider text-neutral-500">
+          I'm currently a graduate student at the University of Oklahoma, where
+          I'm pursuing a Master&apos;s of Sciecne in Computer Science. I intend
+          to focus on applying machine learning and data mining to physics-based
+          weather analytics and modeling, particularly in the prediction of
+          high-impact weather events.
+        </p>
+      </div>
+    </>
   );
 }
